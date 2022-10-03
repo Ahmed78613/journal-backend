a0c217cd35b985c08f90f653f8c74b2d88516f3c
“thamiem-n” <thamiem123@hotmail.com>
2022-10-03 17:11:06 +0100

    completed backend

--- controllers/journal.js
+++ controllers/journal.js
@@ -53,4 +53,121 @@ const addNewComment = async (req, res) => {
 	}
 };
 
-module.exports = { getAllJournals, postNewJournal, addNewComment };
+const getJournal = async (req, res) => {
+	const { id } = req.params;
+	try {
+		const jsonString = await fs.readFileSync("./data.json", "utf-8");
+		const journal = await JSON.parse(jsonString);
+		
+		//filter journal by id 
+		const specifcJournal  = journal.filter((journal) => id === journal.id)	
+			if(specifcJournal.length>0){
+				res.send(specifcJournal[0]);
+			}else{
+				res.send("error - id does not exist")
+			}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+
+
+
+// const emojiCounter = async (req, res) => {
+// 	const { id , emoji} = req.params; 
+
+// 	try {
+// 		const jsonString = await fs.readFileSync("./data.json", "utf-8");
+// 		const journal = await JSON.parse(jsonString);
+// 		// console.log(emoji)
+
+
+// 		if(emoji == "one"){
+
+// 			const emojis = journal.map((ele) => {
+// 				if (id === ele.id) {
+// 					// console.log(ele.emoji[0])
+// 					return {
+// 						...ele,
+// 						emoji: [
+// 							...ele.emoji, 
+// 							{
+// 								emojiUsed : ele.emoji[0].emojiUsed ? false : true,
+// 							 	emojiOne: ele.emoji[0].emojiUsed ? ele.emoji[0].emojiOne - 1 : ele.emoji[0].emojiOne + 1
+// 							}
+// 						]	
+// 					}
+// 					// return {
+// 					// 	...journal, 
+// 					// 	comments: [ body, ...journal.comments]
+// 					// }
+// 				}
+// 			});
+// 			console.log(emojis[0].emoji)
+
+// 		}else if(emoji == "two"){
+
+// 		}else if(emoji == "three"){
+
+// 		}else{
+// 			res.send("nothing")
+// 		}
+
+
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// };
+
+
+
+const emojiCounter = async (req, res) => {
+	const { id , emoji} = req.params; 
+
+	try {
+		const jsonString = await fs.readFileSync("./data.json", "utf-8");
+		const journal = await JSON.parse(jsonString);
+		// console.log(emoji)
+			const updatedEmoji = journal.map((ele) => {
+				if (id === ele.id) {
+					if(emoji === "one"){
+						ele.emoji[0].emojiUsed ? ele.emoji[0].emojiUsed = false : ele.emoji[0].emojiUsed = true;
+						ele.emoji[0].emojiUsed ? ele.emoji[0].emojiOne +=1 : ele.emoji[0].emojiOne -=1 ;	
+						return ele; 
+					}else if( emoji === "two"){
+						ele.emoji[1].emojiUsed ? ele.emoji[1].emojiUsed = false  : ele.emoji[1].emojiUsed = true;
+						ele.emoji[1].emojiUsed ? ele.emoji[1].emojiTwo +=1 : ele.emoji[1].emojiTwo -=1 ;	
+						return ele; 
+					}else if(emoji === "three"){
+						ele.emoji[2].emojiUsed ? ele.emoji[2].emojiUsed = false  : ele.emoji[2].emojiUsed = true;
+						ele.emoji[2].emojiUsed ? ele.emoji[2].emojiThree +=1 : ele.emoji[2].emojiThree -=1 ;	
+						return ele; 
+					}else return ele; 		
+				}
+			});
+
+			// update data.json file with new data
+			fs.writeFileSync("./data.json", JSON.stringify(updatedEmoji, null, 2));
+			res.send("Emoji count updated");
+
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { getAllJournals, postNewJournal, addNewComment, getJournal, emojiCounter};
--- routes/journal.js
+++ routes/journal.js
@@ -4,10 +4,18 @@ const {
 	getAllJournals,
 	postNewJournal,
 	addNewComment,
+	getJournal,
+	emojiCounter
 } = require("../controllers/journal");
 
 router.get("/", getAllJournals);
 router.post("/", postNewJournal);
-router.post("/new-comment/:id", addNewComment);
+router.post("/:id/new-comment", addNewComment);
+router.get("/:id", getJournal );
+router.post("/:id/emoji/:emoji", emojiCounter);
+
+
+
+
 
 module.exports = router;
